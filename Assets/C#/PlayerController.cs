4f85ee08ebb0161ea31c22f0a7ecd050ab2dcd4f
miketranh <miketranh21@gmail.com>
2016-06-02 10:15:41 -0400

    Change 3
    Camera Controller and finally lighting, Ball movement as well

--- Assets/C#/PlayerController.cs
+++ Assets/C#/PlayerController.cs
@@ -3,14 +3,24 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void FixedUpdate () 
-	{
-	Input.GetAxis
-	}
+	public float speed;
 	
-	// Update is called once per frame
-	void Update () {
+	private Rigidbody rb;
+		
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+    void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+	
+		rb.AddForce (movement * speed);
 	
 	}
 }
+
